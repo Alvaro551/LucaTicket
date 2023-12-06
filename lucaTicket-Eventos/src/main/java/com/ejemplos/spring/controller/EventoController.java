9d30c7c08465b9d4fdc9b06ef214c5d7f98ac843
Víctor <victor.greciano1@gmail.com>
2023-12-06 08:39:41 +0100

    gola

--- lucaTicket-Eventos/src/main/java/com/ejemplos/spring/controller/EventoController.java
+++ lucaTicket-Eventos/src/main/java/com/ejemplos/spring/controller/EventoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +23,8 @@ import com.ejemplos.spring.response.EventoResponse;
 import com.ejemplos.spring.service.EventoService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * El controlador EventoController maneja las solicitudes relacionadas con los
