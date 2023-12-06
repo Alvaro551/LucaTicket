b22c8d0aa4f1eb70da0ca6733c3d9f693c545824
carpio <adriangarciacarpio3@gmail.com>
2023-12-06 11:48:42 +0100

    adri

--- lucaTicket-Eventos/src/main/java/com/ejemplos/spring/controller/EventoController.java
+++ lucaTicket-Eventos/src/main/java/com/ejemplos/spring/controller/EventoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -151,6 +152,7 @@ public class EventoController {
 				.collect(Collectors.toList());
 		return ResponseEntity.ok(CustomResponse.createSuccessResponse(eventosGeneroResponse));
 	}
+	
  /* 
   @PutMapping("/{id}")
 	public ResponseEntity<CustomResponse<EventoResponse>> editarEvento(@RequestBody Eventos evento, @PathVariable Integer id){
@@ -163,6 +165,7 @@ public class EventoController {
     	return ResponseEntity.ok(CustomResponse.createSuccessResponse(eventosGeneroResponse));
     }
 	*/
+	
 	@PutMapping("/{id}")
 	public ResponseEntity<CustomResponse<EventoResponse>> editarEvento(@RequestBody EventosRequest eventoRequest, @PathVariable Integer id){
 		Eventos evento = eventoRequest.transformToEventos();
