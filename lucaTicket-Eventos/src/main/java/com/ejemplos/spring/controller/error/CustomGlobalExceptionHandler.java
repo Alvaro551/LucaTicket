9b5c8eb99990520047bfffd6978c5b3e4d26eb85
Alvaro551 <alvaromedinavena11@gmail.com>
2023-12-12 18:32:52 +0100

    Manejo de errores de usuarios

--- lucaTicket-Eventos/src/main/java/com/ejemplos/spring/controller/error/CustomGlobalExceptionHandler.java
+++ lucaTicket-Eventos/src/main/java/com/ejemplos/spring/controller/error/CustomGlobalExceptionHandler.java
@@ -1,106 +1,12 @@
 package com.ejemplos.spring.controller.error;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import com.ejemplos.spring.model.CustomResponse;
-
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * La clase CustomGlobalExceptionHandler es un manejador de excepciones global para personalizar las respuestas de error.
  */
-@ControllerAdvice
-public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+@RestControllerAdvice
+public class CustomGlobalExceptionHandler  {
 
-    // @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(EventoNotFoundException.class)
-    public void springHandleNotFound(HttpServletResponse response) throws IOException {
-        logger.info("------ JuegoNotFoundException() ");
-        // Saltará a la clase CustomErrorAttributes para crear un error personalizado
-        response.sendError(HttpStatus.NOT_FOUND.value());
-    }
-
-    // @Validate For Validating Path Variables and Request Parameters
-    @ExceptionHandler(ConstraintViolationException.class)
-    public void constraintViolationException(HttpServletResponse response) throws IOException {
-        logger.info("------ ConstraintViolationException() ");
-        response.sendError(HttpStatus.BAD_REQUEST.value());
-    }
-    
-	// Manejo de errores para tipo de fecha y hora incorrectas
-	@SuppressWarnings("unchecked")
-	@ExceptionHandler(DateTimeParseException.class)
-	public ResponseEntity<CustomResponse> handleDateTimeParseException(DateTimeParseException ex) {
-		CustomResponse response = new CustomResponse(500,
-				"Error: La fecha tiene un formato incorrecto. Debe ser dd-MM-yyyy", HttpStatus.BAD_REQUEST);
-		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-	}
-
-    // Manejo de errores para @Valid
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-            HttpHeaders headers, HttpStatus status, WebRequest request) {
-
-        logger.info("------ handleMethodArgumentNotValid()");
-
-        CustomErrorJson customError = new CustomErrorJson();
-
-        customError.setTimestamp(new Date());
-        customError.setStatus(status.value());
-        customError.setError(status.name());
-
-        List<String> messages = new ArrayList<String>();
-        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
-            messages.add(error.getField() + ": " + error.getDefaultMessage());
-        }
-        customError.setMessage(messages);
-
-        // Para recoger el path y simular de forma completa los datos originales
-        // request.getDescription(false) ---> uri=/students
-        String uri = request.getDescription(false);
-        uri = uri.substring(uri.lastIndexOf("=") + 1);
-        customError.setPath(uri);
-
-        return new ResponseEntity<>(customError, headers, status);
-
-    }
-
-    // Manejo de errores para HttpRequestMethodNotSupported
-    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
-            HttpHeaders headers, HttpStatus status, WebRequest request) {
-        logger.info("------ handleHttpRequestMethodNotSupported()");
-        StringBuilder builder = new StringBuilder();
-        builder.append(ex.getMethod());
-        builder.append(" method is not supported for this request. Supported methods are ");
-        ex.getSupportedHttpMethods().forEach(t -> builder.append(t + " "));
-
-        Map<String, Object> body = new LinkedHashMap<>();
-        // Paso fecha formateada a String
-        body.put("timestamp", new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()));
-        body.put("status", status.value());
-        body.put("error", ex.getLocalizedMessage());
-        body.put("message", builder.toString());
-        body.put("autor", "Alvaro");
-
-        return new ResponseEntity<Object>(body, new HttpHeaders(), HttpStatus.METHOD_NOT_ALLOWED);
-
-    }
+   
 }
--- lucaTicket-Usuarios/src/main/java/com/ejemplos/spring/controller/UsuariosController.java
+++ lucaTicket-Usuarios/src/main/java/com/ejemplos/spring/controller/UsuariosController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,117 +36,108 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 @RequestMapping("/usuarios")
 public class UsuariosController {
 
-    @Autowired
-    private UsuarioService usuarioService;
+	@Autowired
+	private UsuarioService usuarioService;
 
-    @Autowired
-    private UsuariosRepository usuarioRepository;
+	@Autowired
+	private UsuariosRepository usuarioRepository;
 
-    /**
-     * Obtiene todos los usuarios.
-     *
-     * @return ResponseEntity con la lista de UsuarioResponse y el código de estado correspondiente.
-     */
-    @GetMapping
-    public ResponseEntity<CustomResponse<List<UsuarioResponse>>> obtenerUsuarios() {
-        List<Usuario> usuarios = usuarioRepository.findAll();
-        List<UsuarioResponse> usuarioResponses = usuarios.stream()
-                                                         .map(UsuarioResponse::of)
-                                                         .collect(Collectors.toList());
-        return ResponseEntity.ok(CustomResponse.createSuccessResponse(usuarioResponses));
-    }
+	/**
+	 * Obtiene todos los usuarios.
+	 *
+	 * @return ResponseEntity con la lista de UsuarioResponse y el código de estado
+	 *         correspondiente.
+	 */
+	@GetMapping
+	public ResponseEntity<CustomResponse<List<UsuarioResponse>>> obtenerUsuarios() {
+		List<Usuario> usuarios = usuarioRepository.findAll();
+		List<UsuarioResponse> usuarioResponses = usuarios.stream().map(UsuarioResponse::of)
+				.collect(Collectors.toList());
+		return ResponseEntity.ok(CustomResponse.createSuccessResponse(usuarioResponses));
+	}
 
-    /**
-     * Agrega un nuevo usuario.
-     *
-     * @param nuevoUsuario El nuevo usuario a agregar.
-     * @return ResponseEntity con el UsuarioResponse del usuario agregado y el código de estado correspondiente.
-     */
-    @PostMapping
-    public ResponseEntity<CustomResponse<UsuarioResponseADD>> addUsuario(@RequestBody UsuarioRequest nuevoUsuarioRequest) {
-        try {
-            Usuario nuevoUsuario = nuevoUsuarioRequest.transformToUsuario();
-            Usuario usuarioGuardado = usuarioService.addUsuario(nuevoUsuario);
-            return ResponseEntity.status(HttpStatus.CREATED)
-                                 .body(CustomResponse.createSuccessResponse(UsuarioResponseADD.of(usuarioGuardado)));
-            
-        } catch (DataIntegrityViolationException e) {
-            // Aquí capturas la excepción específica y envías una respuesta personalizada
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-                                 .body(CustomResponse.createConflictResponse("El correo electrónico ya está en uso",null));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                                 .body(CustomResponse.createInternalServerErrorResponse("Error al crear el usuario"));
-        }
-    }
+	/**
+	 * Agrega un nuevo usuario.
+	 *
+	 * @param nuevoUsuario El nuevo usuario a agregar.
+	 * @return ResponseEntity con el UsuarioResponse del usuario agregado y el
+	 *         código de estado correspondiente.
+	 */
+	@PostMapping
+	public ResponseEntity<CustomResponse<UsuarioResponseADD>> addUsuario(@RequestBody UsuarioRequest nuevoUsuarioRequest) {
+	    Usuario nuevoUsuario = nuevoUsuarioRequest.transformToUsuario();
+	    Usuario usuarioGuardado = usuarioService.addUsuario(nuevoUsuario);
+	    return ResponseEntity.status(HttpStatus.CREATED)
+	                         .body(CustomResponse.createSuccessResponse(UsuarioResponseADD.of(usuarioGuardado)));
+	}
 
-    
-    
-    /**
-     * Borra un usuario por su ID.
-     *
-     * @param id El identificador único del Usuario a borrar.
-     * @return ResponseEntity con el CustomResponse que contiene el usuario antes de ser borrado y el código de estado correspondiente.
-     */
-    @DeleteMapping("/{id}")
-    @Operation(summary = "Borrar un usuario por ID", description = "Elimina un usuario de la base de datos basado en su ID")
-    @ApiResponse(responseCode = "200", description = "Usuario eliminado con éxito")
-    public ResponseEntity<CustomResponse<UsuarioResponseADD>> borrarUsuarioPorId(@PathVariable Integer id) {
-        try {
-            // Intenta obtener el usuario antes de borrarlo
-            Usuario usuarioAntes = usuarioService.findById(id);
+	
 
-            // Intenta borrar el usuario
-            boolean borradoExitoso = usuarioService.borrarUsuarioPorId(id);
+	/**
+	 * Borra un usuario por su ID.
+	 *
+	 * @param id El identificador único del Usuario a borrar.
+	 * @return ResponseEntity con el CustomResponse que contiene el usuario antes de
+	 *         ser borrado y el código de estado correspondiente.
+	 */
+	@DeleteMapping("/{id}")
+	@Operation(summary = "Borrar un usuario por ID", description = "Elimina un usuario de la base de datos basado en su ID")
+	@ApiResponse(responseCode = "200", description = "Usuario eliminado con éxito")
+	public ResponseEntity<CustomResponse<UsuarioResponseADD>> borrarUsuarioPorId(@PathVariable Integer id) {
+		try {
+			// Intenta obtener el usuario antes de borrarlo
+			Usuario usuarioAntes = usuarioService.findById(id);
 
-            if (borradoExitoso) {
-                return ResponseEntity.ok(CustomResponse.createSuccessResponse(UsuarioResponseADD.of(usuarioAntes)));
-            } else {
-                // Si el usuario no existe o no pudo ser borrado
-                return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(CustomResponse.createNotFoundResponse("Usuario no encontrado"));
-            }
-        } catch (DateTimeParseException e) {
-            // Manejar la excepción de formato de fecha incorrecto
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(CustomResponse.createBadRequestResponse("Formato de fecha incorrecto"));
-        } catch (Exception e) {
-            // Manejar otras excepciones inesperadas
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(CustomResponse.createInternalServerErrorResponse("Error interno al borrar el usuario"));
-        }
-    }
-    
-    /**
-     * Edita un usuario por su ID.
-     *
-     * @param id El identificador único del Usuario a editar.
-     * @param usuarioNuevo El usuario con los datos actualizados.
-     * @return ResponseEntity con el CustomResponse que contiene el usuario actualizado y el código de estado correspondiente.
-     */
-    @PutMapping("/{id}")
-    @Operation(summary = "Editar un usuario por ID", description = "Actualiza un usuario en la base de datos basado en su ID")
-    @ApiResponse(responseCode = "200", description = "Usuario editado con éxito")
-    public ResponseEntity<CustomResponse<UsuarioResponseADD>> editarUsuario(
-            @PathVariable("id") Integer id,
-            @RequestBody UsuarioRequest usuarioNuevoRequest) {
-        try {
-            Usuario usuarioNuevo = usuarioNuevoRequest.transformToUsuario();
-            Usuario usuarioEditado = usuarioService.editarUsuario(id, usuarioNuevo);
+			// Intenta borrar el usuario
+			boolean borradoExitoso = usuarioService.borrarUsuarioPorId(id);
 
-            if (usuarioEditado != null) {
-                return ResponseEntity.ok(CustomResponse.createSuccessResponse(UsuarioResponseADD.of(usuarioEditado)));
-            } else {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(CustomResponse.createNotFoundResponse("Usuario no existente"));
-            }
-        } catch (DateTimeParseException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(CustomResponse.createBadRequestResponse("Formato de fecha incorrecto"));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(CustomResponse.createInternalServerErrorResponse("Error interno al editar el usuario"));
-        }
-    }
+			if (borradoExitoso) {
+				return ResponseEntity.ok(CustomResponse.createSuccessResponse(UsuarioResponseADD.of(usuarioAntes)));
+			} else {
+				// Si el usuario no existe o no pudo ser borrado
+				return ResponseEntity.status(HttpStatus.NOT_FOUND)
+						.body(CustomResponse.createNotFoundResponse("Usuario no encontrado"));
+			}
+		} catch (DateTimeParseException e) {
+			// Manejar la excepción de formato de fecha incorrecto
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(CustomResponse.createBadRequestResponse("Formato de fecha incorrecto"));
+		} catch (Exception e) {
+			// Manejar otras excepciones inesperadas
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(CustomResponse.createInternalServerErrorResponse("Error interno al borrar el usuario"));
+		}
+	}
+
+	/**
+	 * Edita un usuario por su ID.
+	 *
+	 * @param id           El identificador único del Usuario a editar.
+	 * @param usuarioNuevo El usuario con los datos actualizados.
+	 * @return ResponseEntity con el CustomResponse que contiene el usuario
+	 *         actualizado y el código de estado correspondiente.
+	 */
+	@PutMapping("/{id}")
+	@Operation(summary = "Editar un usuario por ID", description = "Actualiza un usuario en la base de datos basado en su ID")
+	@ApiResponse(responseCode = "200", description = "Usuario editado con éxito")
+	public ResponseEntity<CustomResponse<UsuarioResponseADD>> editarUsuario(@PathVariable("id") Integer id,
+			@RequestBody UsuarioRequest usuarioNuevoRequest) {
+		try {
+			Usuario usuarioNuevo = usuarioNuevoRequest.transformToUsuario();
+			Usuario usuarioEditado = usuarioService.editarUsuario(id, usuarioNuevo);
+
+			if (usuarioEditado != null) {
+				return ResponseEntity.ok(CustomResponse.createSuccessResponse(UsuarioResponseADD.of(usuarioEditado)));
+			} else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND)
+						.body(CustomResponse.createNotFoundResponse("Usuario no existente"));
+			}
+		} catch (DateTimeParseException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(CustomResponse.createBadRequestResponse("Formato de fecha incorrecto"));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(CustomResponse.createInternalServerErrorResponse("Error interno al editar el usuario"));
+		}
+	}
 }
-
