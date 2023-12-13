2ec12efb52a8c09a58a743d97b61e49b6aa91509
Alvaro551 <alvaromedinavena11@gmail.com>
2023-12-13 18:20:54 +0100

    HotFix

--- lucaTicket-Usuarios/src/main/java/com/ejemplos/spring/controller/UsuariosController.java
+++ lucaTicket-Usuarios/src/main/java/com/ejemplos/spring/controller/UsuariosController.java
@@ -5,10 +5,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +26,7 @@ import com.ejemplos.spring.services.UsuarioService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 /**
  * Controlador para gestionar las operaciones relacionadas con usuarios.
@@ -64,6 +63,12 @@ public class UsuariosController {
 	 *         código de estado correspondiente.
 	 */
 	@PostMapping
+	@Operation(summary = "Añadir un nuevo usuario", description = "Crea y guarda un nuevo usuario en la base de datos")
+	@ApiResponses(value = { 
+	    @ApiResponse(responseCode = "201", description = "Usuario creado con éxito"),
+	    @ApiResponse(responseCode = "400", description = "Solicitud inválida si los datos del usuario no son correctos"),
+	    @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+	})
 	public ResponseEntity<CustomResponse<UsuarioResponseADD>> addUsuario(@RequestBody UsuarioRequest nuevoUsuarioRequest) {
 	    Usuario nuevoUsuario = nuevoUsuarioRequest.transformToUsuario();
 	    Usuario usuarioGuardado = usuarioService.addUsuario(nuevoUsuario);
